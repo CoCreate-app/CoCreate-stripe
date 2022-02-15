2c5dc0598462508a45b12c3ff2e3d0df8f6844ec
Frank <frank@cocreate.app>
2022-02-15 18:14:23 +0000

    fix: reduced code, get environment from db

--- src/client.js
+++ src/client.js
@@ -2,95 +2,18 @@ import api from "@cocreate/api";
 
 const CoCreateStripe = {
     name: "stripe",
-    actions: [
-        "balanceTransaction",
-        "createSourceCustomer",
-        "createCustomer",
-        "getBalance",
-        "listCustomers",
-        "createTokenCard",
-        "createCustomer",
-        "updateCustomer",
-        "createCharge",
-        "create",
-    ],
-
-    render_create: function (data) {
-        if (data.object == "error") {
-            alert(data.data);
-        }
-        data = { data: data };
-        console.log("data ", data);
-    },
-    render_createCharge: function (data) {
-        if (data.object == "error") {
-            alert(data.data);
-        }
-        data = { data: data };
-        console.log("data ", data);
-    },
-    render_updateCustomer: function (data) {
-        if (data.object == "error") {
-            alert(data.data);
-        }
-        data = { data: data };
-        console.log("data ", data);
-    },
-
-    render_createCustomer: function (data) {
-        if (data.object == "error") {
-            alert(data.data);
-        }
-        data = { data: data };
-        console.log("data ", data);
-    },
-
-    render_createTokenCard: function (data) {
-        if (data.object == "error") {
-            alert(data.data);
-        }
-        data = { data: data };
-        console.log("createTokenCard ", data);
-        CoCreate.api.render("createTokenCard", data);
-    },
-    render_balanceTransaction: function (data) {
-        if (data.object == "error") {
-            alert(data.data);
-        }
-        data = { data: data };
-        console.log("data ", data);
-    },
-    render_createSourceCustomer: function (data) {
-        if (data.object == "error") {
-            alert(data.data);
-        }
-        data = { data: data };
-        console.log("data ", data);
-    },
-    render_getBalance: function (data) {
-        if (data.object == "error") {
-            alert(data.data);
-        }
-        data = { data: data };
-        console.log("data ", data);
-        CoCreate.api.render("getBalance", data);
-    },
-    render_createCustomer: function (data) {
-        if (data.object == "error") {
-            alert(data.data);
-        }
-        data = { data: data };
-        console.log("data ", data);
-        CoCreate.api.render("createCustomer", data);
-    },
-    render_listCustomers: function (data) {
-        if (data.object == "error") {
-            alert(data.data);
-        }
-        data = { data: data };
-        console.log("data ", data);
-        CoCreate.api.render("listCustomers", data);
-    },
+    actions: {
+        'balance.retrieve': {},
+        'balanceTransactions.retrieve': {},
+        'balanceTransactions.list': {},
+        createSourceCustomer: {},
+        'customers.create': {},
+        'customers.list': {},
+        'customers.update':{},
+        'charges.create':{},
+        'tokens.create':{},
+        create:{}
+    }
 };
 
 api.init({
--- src/server.js
+++ src/server.js
@@ -1,143 +1,89 @@
 'use strict'
-const  api = require("@cocreate/api");
+const api = require("@cocreate/api");
 
 class CoCreateStripe {
-	constructor(wsManager, config) {
-		this.moduleName = 'stripe';
-		this.enviroment = 'test'; //'test'
-		this.wsManager = wsManager;
-		this.init();
-	}
-	
-	init() {
-		if (this.wsManager) {
-		  this.wsManager.on('stripe',	(socket, data, roomInfo) => this.sendStripe(socket, data, roomInfo));
-		}
-	}
+  constructor(wsManager, config) {
+    this.moduleName = 'stripe';
+    this.wsManager = wsManager;
+    this.init();
+  }
+
+  init() {
+    if (this.wsManager) {
+      this.wsManager.on('stripe', (socket, data, roomInfo) => this.sendStripe(socket, data, roomInfo));
+    }
+  }
 
   async sendStripe(socket, data, roomInfo) {
-    let that = this;
-    let send_response ='stripe';
-    let data_original = {...data};
-    const params = data['data'];
+    let params = data['data'];
+    let environment;
     let type = data['type'];
-    delete data['type'];
-    let url = '';
-    let method = '';
-    let targets = [];
-    let tags = [];
     let stripe = false;
-    try{
-      let enviroment = typeof params['enviroment'] != 'undefined' ? params['enviroment'] : this.enviroment;
-      let org = await api.getOrg(params, this.moduleName);
-      let key = org['apis.'+this.moduleName+'.'+enviroment];
-      console.log("stripe ", type, key)
+
+    try {
+      let org = await api.getOrg(data, this.moduleName);
+      if (params.environment){
+        environment = params['environment'];
+        delete params['environment'];  
+      } else {
+        environment = org['apis.' + this.moduleName + '.environment'];
+      }
+      
+      let key = org['apis.' + this.moduleName + '.' + environment];
       stripe = require('stripe')(key);
-    }catch(e){
-      console.log(this.moduleName+" : Error Connect to api",e)
-      return false;
+    } catch (e) {
+      console.log(this.moduleName + " : Error Connect to api", e)
     }
 
-    switch (type) {
-      case 'listCustomers':
-          const customers = await stripe.customers.list();
-          api.send_response(this.wsManager, socket, { "type": data_original["type"], "response": customers }, this.moduleName)
-      break;
-      case 'createCharge':
-          this.createCharge(socket, type, data["data"], stripe);
-      break;
-      case 'createCustomer':
-          this.createCustomer(socket, type, data["data"], stripe);
-      break;
-      case 'updateCustomer':
-          this.updateCustomer(socket, type, data["data"], stripe);
-      break;
-      case 'createTokenCard':
-          this.createTokenCard(socket, type, data["data"], stripe);
-      break;
-      case 'getBalance':
-          console.log("Response getBalance Before")
-          stripe.balance.retrieve((err, balance) => {
-              if (!err && balance) {
-                api.send_response(this.wsManager, socket, { "type": data_original["type"], "response": balance }, this.moduleName)
-              } else if (err) {
-                  api.send_response(this.wsManager, socket, { "type": data_original["type"], "response": 0 }, this.moduleName)
-              }
-            });
+    try {
+      let response, customer = params.customer;
+      switch (type) {
+        case 'customers.list':
+          response = await stripe.customers.list();
           break;
-      case 'balanceTransaction':
-          const balanceTransaction = await stripe.balanceTransactions.retrieve(
-            'txn_1032HU2eZvKYlo2CEPtcnUvl'
+        case 'customers.create':
+          response = await stripe.customers.create(params);
+          break;
+        case 'customers.update':
+          delete params['customer'];
+          response = await stripe.customers.update(customer, params);
+          break;
+        case 'tokens.create':
+          response = await stripe.tokens.create(params);
+          break;
+        case 'charges.create':
+          response = await stripe.charges.create(params);
+          break;
+        case 'balance.retrieve':
+          response = await stripe.balance.retrieve();
+          break;
+        case 'balanceTransactions.retrieve':
+          response = await stripe.balanceTransactions.retrieve(
+            params.balance_transaction
           );
-          api.send_response(this.wsManager, socket, { "type": data_original["type"], "response": balanceTransaction }, this.moduleName)
-      break;
-      case 'createCustomer':
-          const customer = await stripe.customers.create(data["data"]);
-          //console.log(" REspuesta ",customer)
-          api.send_response(this.wsManager, socket, { "type": data_original["type"], "response": customer }, this.moduleName)
-      break;
-      case 'createSourceCustomer':
-          let id_customer = data['customer_id'];
-          delete data['customer_id'];
-          const card = await stripe.customers.createSource(
-            id_customer,
-            data["data"]
-          );
-          api.send_response(this.wsManager, socket, { "type": data_original["type"], "response": card }, this.moduleName)
-      break;
-    } 
-	}// end sendStripe
-	
-	async createCharge(socket, type, params, stripelib) {
-	    try {
-	        const result = await stripelib.charges.create(params);
-	        api.send_response(this.wsManager, socket, { "type": type, "response": result }, this.moduleName);
-        } catch (error) {
-          this.handleError(socket, type, error)
-        }
-	}
-	
-	async updateCustomer(socket, type, params, stripelib) {
-	    try {
-	        let customer_id = params['customer_id']
-	        delete params['customer_id'];
-	        const result = await stripelib.customers.update(
-              customer_id,
-              {...params}
-            );
-            api.send_response(this.wsManager, socket, { "type": type, "response": result }, this.moduleName);
-        } catch (error) {
-          this.handleError(socket, type, error)
-        }
-	}
-	async createCustomer(socket, type, params, stripelib) {
-        try {
-            const result = await stripelib.customers.create(params);
-            api.send_response(this.wsManager, socket, { "type": type, "response": result }, this.moduleName);
-        } catch (error) {
-          this.handleError(socket, type, error)
-        }	    
-	}
-	
-	async createTokenCard(socket, type, params, stripelib) {
-        try {
-            const token = await stripelib.tokens.create({
-              card: params,
-            });
-            api.send_response(this.wsManager, socket, { "type": type, "response": token }, this.moduleName)
+          break;
+        case 'balanceTransactions.list':
+          response = await stripe.balanceTransactions.list();
+          break;
+        case 'createSourceCustomer':
+          delete params['customer'];
+          response = await stripe.customers.createSource(customer, params);
+          break;
+      }
+      this.wsManager.send(socket, this.moduleName, { type, response })
     
-        } catch (error) {
-          this.handleError(socket, type, error)
-        }
+    } catch (error) {
+      this.handleError(socket, type, error)
     }
-      
-    handleError(socket, type, error) {
-        const response = {
-          'object': 'error',
-          'data':error || error.response || error.response.data || error.response.body || error.message || error,
-        };
-        api.send_response(this.wsManager, socket, { type, response }, this.moduleName);
-    }
+  }// end sendStripe
+
+  handleError(socket, type, error) {
+    const response = {
+      'object': 'error',
+      'data': error || error.response || error.response.data || error.response.body || error.message || error,
+    };
+    this.wsManager.send(socket, this.moduleName, { type, response })
+  }
 }
 
 module.exports = CoCreateStripe;
