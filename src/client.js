5df60e0a190e2692e424f5f024c917e3c10449f6
frankpagan <frank@cocreate.app>
2024-02-01 11:27:55 -0500

    fix:  removed from clinet.js

--- src/client.js
+++ src/client.js
@@ -1,18 +1,18 @@
 import api from "@cocreate/api";
 
-api.init({
-    name: "stripe",
-    endPoints: {
-        'accounts.create': {},
-        'balance.retrieve': {},
-        'balanceTransactions.retrieve': {},
-        'balanceTransactions.list': {},
-        createSourceCustomer: {},
-        'customers.create': {},
-        'customers.list': {},
-        'customers.update': {},
-        'charges.create': {},
-        'tokens.create': {},
-        create: {}
-    }
-});
+// api.init({
+//     name: "stripe",
+//     endPoints: {
+//         'accounts.create': {},
+//         'balance.retrieve': {},
+//         'balanceTransactions.retrieve': {},
+//         'balanceTransactions.list': {},
+//         createSourceCustomer: {},
+//         'customers.create': {},
+//         'customers.list': {},
+//         'customers.update': {},
+//         'charges.create': {},
+//         'tokens.create': {},
+//         create: {}
+//     }
+// });
